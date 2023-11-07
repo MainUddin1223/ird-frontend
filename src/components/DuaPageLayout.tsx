bd5972d2e97e846bf2af87ac3c4c90891494ee96
Md Main Uddin <2021md.main@gmail.com>
2023-11-07 11:34:07 +0600

    test

--- src/components/DuaPageLayout.tsx
+++ src/components/DuaPageLayout.tsx
@@ -1,11 +1,7 @@
 const DuaPageLayout = () => {
-	console.log(err);
 	return (
 		<div>
 			<h1>Dua page layout</h1>
-			<p>Initial layout</p>
-			<p>Initial layout</p>
-			<p>Initial layout</p>
 		</div>
 	);
 };
