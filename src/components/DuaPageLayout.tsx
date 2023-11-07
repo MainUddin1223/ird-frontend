2ef449d4984097cf6a0cacb361049e2f27d599d5
Md Main Uddin <2021md.main@gmail.com>
2023-11-07 20:29:50 +0600

    initial header layout impl

--- src/components/DuaPageLayout.tsx
+++ src/components/DuaPageLayout.tsx
@@ -1,10 +1,13 @@
 import Sidebar from '@/components/Sidebar';
+import Header from './Header';
 
 const DuaPageLayout = () => {
 	return (
 		<div className="flex flex-col-reverse xl:flex-row  xl:m-10">
 			<Sidebar />
-			<div>content</div>
+			<div className="w-full mx-[30px]">
+				<Header />
+			</div>
 		</div>
 	);
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import profileIcon from '@/assets/profile 1.png';
+import dropdownIcon from '@/assets/Polygon 2.png';
+import Image from 'next/image';
+import logo from '@/assets/logo .png';
+import searchIcon from '@/assets/Vector.png';
+const Header = () => {
+	return (
+		<div className="flex justify-between">
+			<div className="">
+				<h1 className="text-2xl font-semibold font-poppins hidden xl:block">
+					Dua page
+				</h1>
+				<div className="flex items-center xl:hidden">
+					<Image
+						src={logo}
+						width={73}
+						height={73}
+						alt="logo"
+						className=" mx-auto cursor-pointer  "
+					/>
+					<h1 className="text-2xl font-semibold font-poppins">
+						Dua and Ruqyah
+					</h1>
+				</div>
+			</div>
+			<div className="w-1/2 xl:w-[630px] flex justify-between">
+				<div className="relative ">
+					<input
+						type="text"
+						placeholder="Search by Dua Name"
+						className="w-[370px] rounded-[10px] text-base p-4 bg-bgColor "
+					/>
+					<div className="absolute right-1 top-1 bg-accent w-54 p-3 rounded-md">
+						<Image src={searchIcon} width={24} height={24} alt="search-icon" />
+					</div>
+				</div>
+				<div className="flex items-center">
+					<div>
+						<Image
+							src={profileIcon}
+							width={45}
+							height={45}
+							alt="profile_icon"
+						/>
+					</div>
+					<div>
+						<Image
+							src={dropdownIcon}
+							width={12}
+							height={9}
+							alt="dropdown_icon"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
